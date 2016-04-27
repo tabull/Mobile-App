4a4c9ce95da41159a74e6db87e241d7f8c5541da
danfozzie <d.j.forrester-14@student.lboro.ac.uk>
2016-04-27 21:42:05 +0100

    Alarm Should work (very shakily)

---
+++ AlarmClockApp/app/src/main/java/com/example/daniel/alarmclockapp/AlarmService.java
@@ -0,0 +1,42 @@
+package com.example.daniel.alarmclockapp;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+/**
+ * Created by Daniel on 27/04/2016.
+ */
+public class AlarmService extends IntentService{
+    private NotificationManager alarmNotificationManager;
+
+    public AlarmService() {
+        super("AlarmService");
+    }
+
+    @Override
+    public void onHandleIntent(Intent intent) {
+        sendNotification("IT FUCKING WORKS BABY!");
+    }
+
+    private void sendNotification(String msg) {
+        alarmNotificationManager = (NotificationManager) this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, alarmList.class), 0);
+
+        NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
+                this).setContentTitle("Alarm").setSmallIcon(R.drawable.ic_launcher)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                .setContentText(msg);
+
+
+        alamNotificationBuilder.setContentIntent(contentIntent);
+        alarmNotificationManager.notify(1, alamNotificationBuilder.build());
+    }
+}
--- AlarmClockApp/app/src/main/java/com/example/daniel/alarmclockapp/settingAlarm.java
+++ AlarmClockApp/app/src/main/java/com/example/daniel/alarmclockapp/settingAlarm.java
@@ -1,24 +1,37 @@
 package com.example.daniel.alarmclockapp;
 
 import android.annotation.TargetApi;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TimePicker;
 
-import java.sql.Time;
+import java.util.Calendar;
+
 
 public class settingAlarm extends AppCompatActivity {
 
+    AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
     public final static String EXTRA_MESSAGE = ".";
+    private static settingAlarm inst;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm_set);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
     }
 
     //Response to button
@@ -28,6 +41,12 @@ public class settingAlarm extends AppCompatActivity {
         timePicker.clearFocus();
         int hour = timePicker.getHour();
         int minute = timePicker.getMinute();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
+        calendar.set(Calendar.MINUTE, timePicker.getMinute());
+        Intent myIntent = new Intent(settingAlarm.this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(settingAlarm.this, 0, myIntent, 0);
+        alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
         Intent intent = new Intent(this, alarmList.class);
         String time = "Alarm set for: " + String.valueOf(hour) + ":" + String.valueOf(minute);
         intent.putExtra(EXTRA_MESSAGE, time);
