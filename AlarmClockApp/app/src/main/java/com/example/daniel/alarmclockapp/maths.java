b26abc8e37c11008c4ed841300dd06cc98e3a87c
tino <t.b.aranjani-14@student.lboro.ac.uk>
2016-05-05 01:59:39 +0100

    maths challenge ( done to a reasonable standard)
    everything works i.e drag&drop, clear button, backspace, check answer.

--- AlarmClockApp/app/src/main/java/com/example/daniel/alarmclockapp/maths.java
+++ AlarmClockApp/app/src/main/java/com/example/daniel/alarmclockapp/maths.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,6 +30,7 @@ public class maths extends AppCompatActivity {
 
 
     @Override
+    //initial declaration of variables
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maths);
@@ -44,8 +46,9 @@ public class maths extends AppCompatActivity {
         findViewById(R.id.txtA4).setOnDragListener(dragListen4);
         findViewById(R.id.txtA5).setOnDragListener(dragListen5);
 
-    }
 
+    }
+    // method used to clear the text boxes
     public void clearBoxes(View view) {
         letter = "";
         textout = (TextView) findViewById(R.id.txtA1);
@@ -55,7 +58,7 @@ public class maths extends AppCompatActivity {
         TextView textout5 = (TextView) findViewById(R.id.txtA5);
 
 
-        if((textout != null || !textout.getText().equals(""))
+        if ((textout != null || !textout.getText().equals(""))
                 && (textout2 != null || !textout2.getText().equals(""))
                 && (textout3 != null || !textout3.getText().equals(""))
                 && (textout4 != null || !textout4.getText().equals(""))
@@ -66,10 +69,10 @@ public class maths extends AppCompatActivity {
             textout3.setText(letter);
             textout4.setText(letter);
             textout5.setText(letter);
-            Toast.makeText(getApplicationContext(), "Cleared",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Cleared", Toast.LENGTH_SHORT).show();
         }
     }
-
+    //method used to clear the boxes one by one (a backspace button)
     public void back1(View view) {
         letter = "";
         textout = (TextView) findViewById(R.id.txtA1);
@@ -78,26 +81,26 @@ public class maths extends AppCompatActivity {
         TextView textout4 = (TextView) findViewById(R.id.txtA4);
         TextView textout5 = (TextView) findViewById(R.id.txtA5);
 
-        if((textout != null || !textout.getText().equals("")) && (textout2 == null || textout2.getText().equals(""))){
+        if ((textout != null || !textout.getText().equals("")) && (textout2 == null || textout2.getText().equals(""))) {
             textout.setText(letter);
         }
-        if((textout != null || !textout.getText().equals("")) && (textout2 != null || !textout2.getText().equals(""))  && (textout3 == null || textout3.getText().equals(""))){
+        if ((textout != null || !textout.getText().equals("")) && (textout2 != null || !textout2.getText().equals("")) && (textout3 == null || textout3.getText().equals(""))) {
             textout2.setText(letter);
         }
-        if((textout != null || !textout.getText().equals("")) && (textout2 != null || !textout2.getText().equals(""))  && (textout3 != null || !textout3.getText().equals(""))
-                && (textout4 == null || textout4.getText().equals("")) ){
+        if ((textout != null || !textout.getText().equals("")) && (textout2 != null || !textout2.getText().equals("")) && (textout3 != null || !textout3.getText().equals(""))
+                && (textout4 == null || textout4.getText().equals(""))) {
             textout3.setText(letter);
         }
-        if((textout != null || !textout.getText().equals("")) && (textout2 != null || !textout2.getText().equals(""))  && (textout3 != null || !textout3.getText().equals(""))
-                && (textout4 != null || !textout4.getText().equals("")) && (textout5 == null || textout5.getText().equals("")) ){
+        if ((textout != null || !textout.getText().equals("")) && (textout2 != null || !textout2.getText().equals("")) && (textout3 != null || !textout3.getText().equals(""))
+                && (textout4 != null || !textout4.getText().equals("")) && (textout5 == null || textout5.getText().equals(""))) {
             textout4.setText(letter);
         }
-        if((textout != null || !textout.getText().equals("")) && (textout2 != null || !textout2.getText().equals(""))  && (textout3 != null || !textout3.getText().equals(""))
-                && (textout4 != null || !textout4.getText().equals("")) && (textout5 != null || !textout5.getText().equals("")) ){
+        if ((textout != null || !textout.getText().equals("")) && (textout2 != null || !textout2.getText().equals("")) && (textout3 != null || !textout3.getText().equals(""))
+                && (textout4 != null || !textout4.getText().equals("")) && (textout5 != null || !textout5.getText().equals(""))) {
             textout5.setText(letter);
         }
     }
-
+    //setting up the drag & drop function
     View.OnLongClickListener longListen = new View.OnLongClickListener() {
         @Override
         public boolean onLongClick(View v) {
@@ -107,17 +110,17 @@ public class maths extends AppCompatActivity {
             return false;
         }
     };
-
+    // dragging of each individual TextView
     View.OnDragListener dragListen1 = new View.OnDragListener() {
         @Override
         public boolean onDrag(View v, DragEvent event) {
             int dragEvent = event.getAction();
             switch (dragEvent) {
                 case DragEvent.ACTION_DRAG_ENTERED:
-                    Log.i("Drag Event", "Entered");
+                    //Log.i("Drag Event", "Entered");
                     break;
                 case DragEvent.ACTION_DRAG_EXITED:
-                    Log.i("Drag Event", "Exited");
+                    //Log.i("Drag Event", "Exited");
                     break;
                 case DragEvent.ACTION_DROP:
                     TextView target = (TextView) v;
@@ -137,10 +140,10 @@ public class maths extends AppCompatActivity {
             int dragEvent = event.getAction();
             switch (dragEvent) {
                 case DragEvent.ACTION_DRAG_ENTERED:
-                    Log.i("Drag Event", "Entered");
+                    //Log.i("Drag Event", "Entered");
                     break;
                 case DragEvent.ACTION_DRAG_EXITED:
-                    Log.i("Drag Event", "Exited");
+                    //Log.i("Drag Event", "Exited");
                     break;
                 case DragEvent.ACTION_DROP:
                     TextView target = (TextView) v;
@@ -160,10 +163,10 @@ public class maths extends AppCompatActivity {
             int dragEvent = event.getAction();
             switch (dragEvent) {
                 case DragEvent.ACTION_DRAG_ENTERED:
-                    Log.i("Drag Event", "Entered");
+                    //Log.i("Drag Event", "Entered");
                     break;
                 case DragEvent.ACTION_DRAG_EXITED:
-                    Log.i("Drag Event", "Exited");
+                    //Log.i("Drag Event", "Exited");
                     break;
                 case DragEvent.ACTION_DROP:
                     TextView target = (TextView) v;
@@ -183,10 +186,10 @@ public class maths extends AppCompatActivity {
             int dragEvent = event.getAction();
             switch (dragEvent) {
                 case DragEvent.ACTION_DRAG_ENTERED:
-                    Log.i("Drag Event", "Entered");
+                    //Log.i("Drag Event", "Entered");
                     break;
                 case DragEvent.ACTION_DRAG_EXITED:
-                    Log.i("Drag Event", "Exited");
+                    //Log.i("Drag Event", "Exited");
                     break;
                 case DragEvent.ACTION_DROP:
                     TextView target = (TextView) v;
@@ -206,10 +209,10 @@ public class maths extends AppCompatActivity {
             int dragEvent = event.getAction();
             switch (dragEvent) {
                 case DragEvent.ACTION_DRAG_ENTERED:
-                    Log.i("Drag Event", "Entered");
+                    //Log.i("Drag Event", "Entered");
                     break;
                 case DragEvent.ACTION_DRAG_EXITED:
-                    Log.i("Drag Event", "Exited");
+                    //Log.i("Drag Event", "Exited");
                     break;
                 case DragEvent.ACTION_DROP:
                     TextView target = (TextView) v;
@@ -222,7 +225,7 @@ public class maths extends AppCompatActivity {
             return true;
         }
     };
-
+    // creating the shadow for the drag (user interface)
     private class DragShadow extends View.DragShadowBuilder {
         ColorDrawable geryBox;
 
@@ -250,7 +253,38 @@ public class maths extends AppCompatActivity {
 
             geryBox.setBounds(0, 0, width, height);
             shadowSize.set(width, height);
-            shadowTouchPoint.set(width/2, height/2);
+            shadowTouchPoint.set(width / 2, height / 2);
+        }
+    }
+
+    // method to check the final answer
+    public void check_ans(View view) {
+
+        TextView txtout1 = (TextView) findViewById(R.id.txtA1);
+        TextView txtout2 = (TextView) findViewById(R.id.txtA2);
+        TextView txtout3 = (TextView) findViewById(R.id.txtA3);
+        TextView txtout4 = (TextView) findViewById(R.id.txtA4);
+        TextView txtout5 = (TextView) findViewById(R.id.txtA5);
+        String txtA1 = txtout1.getText().toString();
+        String txtA2 = txtout2.getText().toString();
+        String txtA3 = txtout3.getText().toString();
+        String txtA4 = txtout4.getText().toString();
+        String txtA5 = txtout5.getText().toString();
+
+        if (txtA1 == null || txtA1.equals("")) {
+            Toast.makeText(getApplicationContext(), "Who are you trying to FOOL!!!", Toast.LENGTH_LONG).show();
+            //Log.i("Result", "null");
+        } else if (txtA1.equals("5") && txtA2.equals("*") && txtA3.equals("2") && txtA4.equals("+") && txtA5.equals("7")) {
+            Log.i("Result", "Successful");
+        } else if (txtA1.equals("2") && txtA2.equals("*") && txtA3.equals("5") && txtA4.equals("+") && txtA5.equals("7")) {
+            Log.i("Result", "Successful");
+        } else if (txtA1.equals("7") && txtA2.equals("+") && txtA3.equals("2") && txtA4.equals("*") && txtA5.equals("5")) {
+            Log.i("Result", "Successful");
+        } else if (txtA1.equals("7") && txtA2.equals("+") && txtA3.equals("5") && txtA4.equals("*") && txtA5.equals("2")) {
+            Log.i("Result", "Successful");
+        } else {
+            Toast.makeText(getApplicationContext(), "WRONG answer, please try again", Toast.LENGTH_LONG).show();
+            Log.i("Result", "ERROR");
         }
     }
 }
